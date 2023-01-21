393465d4ae82fe4b34e9ef372b2ff734ebc53a8c
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2023-01-21 23:23:14 +0530

    added config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Navigate, Routes, Route} from "react-router-dom"
+import HomePage from './scenes/homePage'
+import LoginPage from './scenes/loginPage'
+import ProfilePage from './scenes/profilePage'
 
 function App() {
   const [count, setCount] = useState(0)
