a8aaf1838d3c2509642fda8da8ba9117e5e05274
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2023-01-23 00:37:47 +0530

    navbar added

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,7 +1,86 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/system";
+import {
+  Search,
+  Message,
+  LightMode,
+  DarkMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import FlexBetween from "../../components/FlexBetween";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "./state";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const [isMobileMenuTogged, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("min-width:1000px");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.rimary.light;
+  const alt = heme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
   return (
-    <div>Navbar</div>
-  )
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem,2rem,2.25rem)"
+          color={primary}
+          onClick={() => navigate("/home")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
+          Sociopedia
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="8px"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search">
+              <IconButton>
+                <Search />
+              </IconButton>
+            </InputBase>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <IconButton onCLick={() => dispatch(setMode())}></IconButton>
+        </FlexBetween>
+      ) : (
+        <IconButton></IconButton>
+      )}
+    </FlexBetween>
+  );
 }
